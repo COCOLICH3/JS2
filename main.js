915b449a9cce895a62910fb46ac8fda3635f9a11
Julio Gonzalez <juliogchuburu@gmail.com>
2023-05-22 15:15:41 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/*
 function calcularIva(){
    let tipoIva = prompt("Ingrese el tipo de IVA: A (21%), B (27%), C (42%), D (61%)");
     while(tipoIva != "A" && tipoIva != "B" && tipoIva != "C" && tipoIva != "D" ){
@@ -64,3 +64,191 @@ function agregarProducto(){
 calcularIva();
 alert("--------------------");
 agregarProducto(); 
+
+let saludo = document.getElementById("hola");
+let seccion = prompt('A que seccion desea ir?');
+
+console.log(saludo);
+
+if(seccion === "carrito"){
+    saludo.innerHTML = "<h1>Bienveido al carrito</h1>";
+}
+
+
+
+let nombre = prompt("Ingrese su nombre");
+
+let boton = document.getElementById("boton");
+
+const respuesta = () => {
+    console.log(`Hola ${nombre}`)
+}
+
+boton.addEventListener("click", respuesta);
+
+sessionStorage.setItem("nombre", "julio");
+*/
+class Producto {
+    constructor(nombre, precio, cantidad) {
+      this.nombre = nombre;
+      this.precio = precio;
+      this.cantidad = cantidad;
+    }
+  }
+  const productos = [];
+  
+  function agregarProducto() {
+    for (let i = 1; i <= 2; i++) {
+      let nombre = prompt("Ingrese el nombre");
+      let precio = prompt("Ingrese el precio");
+      let cantidad = prompt("Ingrese la cantidad");
+  
+      let producto = {
+        id: i,
+        nombre,
+        precio,
+        cantidad,
+      };
+  
+      productos.push(producto);
+    }
+  
+    alert("No puede ingresar mas productos");
+  }
+  
+  function mostrarProducto() {
+    productos.forEach((producto) => {
+      alert(
+        `El producto llamado ${producto.nombre}, con cantidad #${producto.cantidad}, tiene como valor ${producto.precio}$`
+      );
+    });
+  }
+  
+  function eliminarProducto() {
+    let eliminar = prompt("Escriba el nombre del producto a eliminar");
+    for (var i = productos.length - 1; i >= 0; i--) {
+      if (productos[i] === eliminar) {
+        productos.splice(i, 1);
+      }
+    }
+    //productos.forEach((producto) => {
+    //const index = productos.indexOf(producto);
+    //console.log(index);
+    //productos.splice(index, 1);
+    //});
+  }
+  
+  function filtrarProducto() {
+    let precioMinimo = parseInt(prompt("Ingrese el precio minimo"));
+    let productosFiltrados = productos.filter(
+      (item) => item.precio > precioMinimo
+    );
+  
+    productosFiltrados.forEach((producto) => {
+      alert(
+        `El producto llamado ${producto.nombre}, con cantidad #${producto.cantidad}, tiene como valor ${producto.precio}$`
+      );
+    });
+  }
+  
+  function verificarProducto() {
+    let verificar = prompt("Escriba el nombre del producto a verificar");
+    productos.forEach((producto) => {
+      alert(productos.some((producto) => producto.nombre === verificar));
+    });
+  }
+  
+  /*
+  function menuInteractivo() {
+    let opcionMenu = prompt(
+      "Elija una opcion, 1 eliminar un producto, 2 agregar otro producto, 3 verificar si un producto existe en la lista"
+    );
+  
+    while (opcionMenu != "1" && opcionMenu != "2" && opcionMenu != "3") {
+      alert("Elija 1, 2 o 3 ");
+      opcionMenu = prompt(
+        "Elija una opcion, 1 eliminar un producto, 2 agregar otro producto, 3 verificar si un producto existe en la lista"
+      );
+    }
+  
+    switch (opcionMenu) {
+      case "1":
+        let eliminar = prompt(
+          "Ingrese el nombre del producto que desea eliminar"
+        );
+        productos.forEach((producto) => {
+          while (eliminar !== producto.nombre) {
+            if (eliminar === producto.nombre) {
+              const index = array.indexOf(producto);
+              productos.splice(index,1);
+                    break;
+          } 
+            else {
+              alert("Producto no encontrado");
+             eliminar = prompt(
+                  "Ingrese el nombre del producto que desea eliminar"
+                );
+            }
+         };
+        }
+        );
+        break;
+  
+      case "2":
+        let agregar = prompt("Ingrese el nombre del producto que desea agregar");
+        productos.forEach((producto) => {
+          while (agregar !== producto.nombre) {
+            if (agregar ===  producto.nombre) {
+              agregarProducto();
+              break;
+            } else {
+              alert("Producto ya existe");
+              agregar = prompt(
+                "Ingrese el nombre del producto que desea agregar"
+              );
+            }
+          }
+        });
+        break;
+        
+        case "3":
+          let verificar = prompt("Ingrese el nombre del producto que desea verificar");
+          productos.forEach((producto) => {
+            while (verificar !== producto.nombre) {
+              if (verificar ===  producto.nombre) {
+                alert(productos.some(producto => producto.nombre === verificar));
+                break;
+              } else {
+                  alert("Producto no existe");
+                verificar = prompt(
+                  "Ingrese el nombre del producto que desea agregar"
+                );
+              }
+            }
+          });
+          break;  
+    }
+  }
+  */
+  
+  alert("Bienvenido, a continuacion ingrese 4 productos");
+  
+  agregarProducto();
+  
+  alert("A continuacion se listaran los productos");
+  
+  mostrarProducto();
+  
+  //eliminarProducto();
+  
+  filtrarProducto();
+  
+  //alert("A continuacion se listaran los productos");
+  
+  //mostrarProducto();
+  
+  verificarProducto();
+  
+  alert("A continuacion se listara la lista final de los productos");
+  
+  mostrarProducto();
